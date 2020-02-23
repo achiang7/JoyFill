b606ea1b0f921bfefaf98e96757060049b8a1a7f
Anthony Chiang <anthonychiang7@gmail.com>
2020-02-22 22:35:01 -0800

    setup ionic proj with firebase, deployed

---
+++ joyfill/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyDRPsK1vtih0GW7GPXOijNe7eQj4Pl73Ro",
+    authDomain: "captstone-joyfill.firebaseapp.com",
+    databaseURL: "https://captstone-joyfill.firebaseio.com",
+    projectId: "captstone-joyfill",
+    storageBucket: "captstone-joyfill.appspot.com",
+    messagingSenderId: "186475823027",
+    appId: "1:186475823027:web:b2891ce89a7654ee4cd5c6",
+    measurementId: "G-L22JCP8NML"
+  }
+};
---
+++ joyfill/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyDRPsK1vtih0GW7GPXOijNe7eQj4Pl73Ro",
+    authDomain: "captstone-joyfill.firebaseapp.com",
+    databaseURL: "https://captstone-joyfill.firebaseio.com",
+    projectId: "captstone-joyfill",
+    storageBucket: "captstone-joyfill.appspot.com",
+    messagingSenderId: "186475823027",
+    appId: "1:186475823027:web:b2891ce89a7654ee4cd5c6",
+    measurementId: "G-L22JCP8NML"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
